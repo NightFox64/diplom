4a8d47c4c9f8ad2c3d5da50dc56b6a8e3e609c0c
NightFox64 <91380334+NightFox64@users.noreply.github.com>
2026-09-22 22:22:10 +0300

    add benchmarks

--- benchmarks/bench_verify.cpp
+++ benchmarks/bench_verify.cpp
@@ -1,9 +1,35 @@
 #include <benchmark/benchmark.h>
 #include "ed25519/fields.hpp"
 
+using namespace ed25519;
+
 static void BM_Sanity(benchmark::State& state) {
     for (auto _ : state) {
         benchmark::DoNotOptimize(ed25519::sanity_check());
     }
 }
 BENCHMARK(BM_Sanity);
+
+static void BM_FieldMul(benchmark::State& state) {
+    FieldElement a = FieldElement::ONE;
+    FieldElement b = FieldElement::ONE;
+    b.limbs[0] = 123456789;
+
+    for (auto _ : state) {
+        a = field_mul(a, b);
+        benchmark::DoNotOptimize(a);
+    }
+}
+BENCHMARK(BM_FieldMul);
+
+static void BM_FieldInv(benchmark::State& state) {
+    FieldElement a = FieldElement::ONE;
+    a.limbs[0] = 987654321;
+
+    for (auto _ : state) {
+        FieldElement res = field_inv(a);
+        benchmark::DoNotOptimize(res);
+        a.limbs[0]++;
+    }
+}
+BENCHMARK(BM_FieldInv);
